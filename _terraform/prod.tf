8cf5fc2dabd18b27a8065fbc8ba1911e81812528
Paweł Dawczak <pawel.dawczak@gmail.com>
2020-05-30 09:04:26 +0100

    Update target_groups config

--- _terraform/prod.tf
+++ _terraform/prod.tf
@@ -173,14 +173,18 @@ resource "aws_lb_listener" "front_web" {
 }
 
 resource "aws_lb_target_group" "front_web" {
-  name     = "front-web-tg"
-  port     = 4000
-  protocol = "HTTP"
-  vpc_id   = aws_default_vpc.default.id
+  name_prefix = "front"
+  port        = 4000
+  protocol    = "HTTP"
+  vpc_id      = aws_default_vpc.default.id
 
-  # health_check {
-  #   path = "/health"
-  # }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  health_check {
+    path = "/health"
+  }
 }
 
 resource "aws_lb_target_group_attachment" "front_web_i1" {
